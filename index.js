cc791a3c065dd066095dd404a64bd123c72fa8db
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-11-24 14:48:09 +0100

    funcionality

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
-var phone = document.getElementById("phone");
-phone === null || phone === void 0 ? void 0 : phone.addEventListener("click", phoneCallVisualize);
-function phoneCallVisualize() {
-    var appsContainer = document.getElementById("appsContainer");
-    var downApp = document.getElementById("downApp");
-    var smartphone = document.getElementById("smartphone");
-    appsContainer === null || appsContainer === void 0 ? void 0 : appsContainer.classList.add("d-none");
-    downApp === null || downApp === void 0 ? void 0 : downApp.classList.add("d-none");
-    smartphone.style.background = "none";
-    smartphone.style.backgroundColor = "white";
-    var rubrica = document.getElementById("rubrica");
-    rubrica === null || rubrica === void 0 ? void 0 : rubrica.classList.add("d-block");
-    rubrica === null || rubrica === void 0 ? void 0 : rubrica.classList.remove("d-none");
-    var call = document.getElementById("call");
-    call === null || call === void 0 ? void 0 : call.addEventListener("click", avvioChiamata);
+window.addEventListener("DOMContentLoaded", code);
+function code() {
+    var User = /** @class */ (function () {
+        function User() {
+            this.numeroChiamate = 0;
+        }
+        User.prototype.ricarica = function () {
+            return Math.floor(Math.random() * 100);
+        };
+        User.prototype.chiamata = function () {
+            var minuti = Math.floor(Math.random() * 10);
+            var costChiamata = minuti * 0.2;
+            return costChiamata;
+        };
+        User.prototype.numero404 = function (ricarica, chiamata) {
+            return ricarica - chiamata;
+        };
+        User.prototype.getNumeroChiamate = function () {
+            this.numeroChiamate++;
+            return this.numeroChiamate;
+        };
+        User.prototype.azzeraChiamate = function (nChiamate) {
+            return nChiamate = 0;
+        };
+        return User;
+    }());
+    var Utente = new User();
+    var phone = document.getElementById("phone");
+    var paypal = document.getElementById("paypal");
+    paypal === null || paypal === void 0 ? void 0 : paypal.addEventListener("click", ricaricaPaypal);
+    var ric = 0;
+    function ricaricaPaypal() {
+        var alertP = document.getElementById("alertP");
+        var divAler = document.getElementsByClassName("alert")[0];
+        var ricarica = Utente.ricarica();
+        console.log("Ricarica di:", ricarica);
+        ric = ricarica;
+        alertP.innerText = "Ricarica effettuata di " + ric.toString() + "€";
+        divAler === null || divAler === void 0 ? void 0 : divAler.classList.add("d-flex");
+        divAler === null || divAler === void 0 ? void 0 : divAler.classList.remove("d-none");
+    }
+    phone === null || phone === void 0 ? void 0 : phone.addEventListener("click", phoneCallVisualize);
+    function phoneCallVisualize() {
+        var divAler = document.getElementsByClassName("alert")[0];
+        divAler === null || divAler === void 0 ? void 0 : divAler.classList.remove("d-block");
+        divAler === null || divAler === void 0 ? void 0 : divAler.classList.add("d-none");
+        var saldoTot = document.getElementById("saldoTot");
+        saldoTot.innerText = "Credito residuo: " + ric.toString() + "€";
+        var appsContainer = document.getElementById("appsContainer");
+        var downApp = document.getElementById("downApp");
+        var smartphone = document.getElementById("smartphone");
+        appsContainer === null || appsContainer === void 0 ? void 0 : appsContainer.classList.add("d-none");
+        downApp === null || downApp === void 0 ? void 0 : downApp.classList.add("d-none");
+        smartphone.style.background = "none";
+        smartphone.style.backgroundColor = "white";
+        var rubrica = document.getElementById("rubrica");
+        rubrica === null || rubrica === void 0 ? void 0 : rubrica.classList.add("d-block");
+        rubrica === null || rubrica === void 0 ? void 0 : rubrica.classList.remove("d-none");
+        var chiamateList = document.getElementById("chiamateList");
+        var call = document.getElementById("call");
+        call === null || call === void 0 ? void 0 : call.addEventListener("click", avvioChiamata);
+        function avvioChiamata() {
+            var li = document.createElement("li");
+            var p = document.createElement("p");
+            var costoChiamata = Utente.chiamata();
+            console.log("Costo chiamata:", costoChiamata);
+            var nChiamate = Utente.getNumeroChiamate();
+            console.log("Index", nChiamate);
+            var ricaricaRimanente = Utente.numero404(ric, costoChiamata);
+            console.log("Ricarica rimanente:", ricaricaRimanente);
+            ric = ricaricaRimanente;
+            saldoTot.innerText = "Credito residuo: " + ricaricaRimanente.toString();
+            p.innerText = "Chiamata Effettuata!" + " Costo: " + costoChiamata + "€";
+            li.appendChild(p);
+            chiamateList === null || chiamateList === void 0 ? void 0 : chiamateList.appendChild(li);
+        }
+    }
 }
--- index.ts
+++ index.ts
@@ -1,27 +1,137 @@
-const phone = document.getElementById("phone") as HTMLAnchorElement | null;
-
-phone?.addEventListener("click", phoneCallVisualize)
-
-function phoneCallVisualize(){
-    const appsContainer = document.getElementById("appsContainer") as HTMLDivElement | null
-    const downApp = document.getElementById("downApp") as HTMLDivElement | null
-    const smartphone = document.getElementById("smartphone") as HTMLDivElement | null 
-
-    appsContainer?.classList.add("d-none")
-    downApp?.classList.add("d-none")
-
-    smartphone!.style.background = "none";
-    smartphone!.style.backgroundColor = "white"
-
-    const rubrica = document.getElementById("rubrica") as HTMLDivElement | null
-    rubrica?.classList.add("d-block")
-    rubrica?.classList.remove("d-none")
+window.addEventListener("DOMContentLoaded", code)
 
 
-    const call = document.getElementById("call") as HTMLAnchorElement | null
-    call?.addEventListener("click", avvioChiamata)
 
-    function avvioChiamata(){
+
+function code(){
+
+
+    
+    interface Smartphone{
+        carica:number;
+        numeroChiamate:number
+    }
+
+    class User implements Smartphone{
+        carica: number;
+        numeroChiamate: number = 0;
+
+        ricarica(){
+            return Math.floor(Math.random()*100)
+        }
+
+        chiamata(){
+            let minuti:number = Math.floor(Math.random()*10)
+            let costChiamata = minuti * 0.2
+            return costChiamata
+        }
+
+        numero404(ricarica, chiamata){
+            return ricarica - chiamata
+        }
+
+        getNumeroChiamate(){
+            this.numeroChiamate++
+            return this.numeroChiamate
+        }
+
+        azzeraChiamate(nChiamate){
+            return nChiamate = 0
+        }
+    }
+    const Utente = new User();
+
+    const phone = document!.getElementById("phone") as HTMLAnchorElement | null;
+
+    const paypal = document.getElementById("paypal") as HTMLAnchorElement | null;
+
+    paypal?.addEventListener("click", ricaricaPaypal)
+
+    let ric = 0;
+
+    function ricaricaPaypal(){
+
+        const alertP = document.getElementById("alertP") as HTMLParagraphElement | null
+        const divAler = document.getElementsByClassName("alert")[0] as HTMLDivElement | null
+
+        let ricarica = Utente.ricarica()
+        console.log("Ricarica di:", ricarica)
+
+        ric = ricarica;
+
+        alertP!.innerText= "Ricarica effettuata di "+ ric.toString() + "€"
+        divAler?.classList.add("d-flex")
+        divAler?.classList.remove("d-none")
+
+        
+    }
+
+    phone?.addEventListener("click", phoneCallVisualize)
+
+    
+    function phoneCallVisualize(){
+
+        const divAler = document.getElementsByClassName("alert")[0] as HTMLDivElement | null
+        divAler?.classList.remove("d-block")
+        divAler?.classList.add("d-none")
+
+        const saldoTot = document.getElementById("saldoTot") as HTMLHeadElement | null
+        saldoTot!.innerText = "Credito residuo: " + ric.toString() +"€";
+    
+        const appsContainer = document.getElementById("appsContainer") as HTMLDivElement | null
+        const downApp = document.getElementById("downApp") as HTMLDivElement | null
+        const smartphone = document.getElementById("smartphone") as HTMLDivElement | null 
+    
+        appsContainer?.classList.add("d-none")
+        downApp?.classList.add("d-none")
+    
+        smartphone!.style.background = "none";
+        smartphone!.style.backgroundColor = "white"
+    
+        const rubrica = document.getElementById("rubrica") as HTMLDivElement | null
+        rubrica?.classList.add("d-block")
+        rubrica?.classList.remove("d-none")
+
+        
+        const chiamateList =  document.getElementById("chiamateList") as HTMLUListElement | null
+
+
+    
+    
+        const call = document.getElementById("call") as HTMLAnchorElement | null
+        call?.addEventListener("click", avvioChiamata)
+    
+        function avvioChiamata(){
+            
+            const li = document.createElement("li")
+
+            const p = document.createElement("p")
+
+            let costoChiamata = Utente.chiamata()
+            console.log("Costo chiamata:", costoChiamata)
+
+            let nChiamate = Utente.getNumeroChiamate()
+            console.log("Index", nChiamate)
+
+            let ricaricaRimanente = Utente.numero404(ric, costoChiamata)
+            console.log("Ricarica rimanente:", ricaricaRimanente)
+
+            ric = ricaricaRimanente
+
+
+            saldoTot!.innerText = "Credito residuo: " + ricaricaRimanente.toString()
+
+            p.innerText = "Chiamata Effettuata!"+" Costo: " + costoChiamata +"€";
+
+            li.appendChild(p)
+
+            chiamateList?.appendChild(li)
+  
+        }
+
+
         
     }
 }
+
+
